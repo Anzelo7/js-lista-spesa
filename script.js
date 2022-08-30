552540321f4648c6f847efb95c5d2525b0c1744c
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-08-30 15:04:42 +0200

    push

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('JS OK')
+
+//stampare una lista della spesa con ciclo while
+
+const list = ['mela', 'pera', 'banana', 'brioche']
+let i = 0;
+
+while (i < list.length) {
+
+    console.log(list[i]);
+
+    i++;
+}
+
+document.innerHTML = list.length
